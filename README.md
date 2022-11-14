5d7a1f6aeb74216a4e88c734f25e8cee01341e9b
Alex Lucchesi <84862301+lucchesia7@users.noreply.github.com>
2022-11-14 11:35:16 -0800

    update readme/push update to pypi
    Updated readme description and pushed package update to pypi

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# funkytown
+# FunkyTown
+
+## Purpose
+A passion project to teach myself how to build and distribute packages to PyPi. 
+
+## Functionality
+FunkyTown has multiple classes to handle Sorting Algorithms, Searching Algorithms, and miscellanious functions 
+for different libraries.
+
+## Installation
+### Windows CMD/OS
+**pip install funkytown**
+
--- pkg/src/funkytown/exp.py
+++ pkg/src/funkytown/exp.py
@@ -2,53 +2,57 @@ from scipy import stats
 import pandas as pd
 import numpy as np
 import ast
-class Sorting_Algo:    
+
+
+class Sorting_Algo:
+
     def __init__(self):
         pass
-    def bubble_sort(self,list: list):
+
+    def bubble_sort(self, list: list):
         """
         Simplist and slowest algorithm used for sorting. Should be used for smaller datasets due to it's BigO Notation.\n
         Designed so that the highest value in the list bubbles its way to the top as the algo iterates through.\n
         Has an outer loop(passes) and an inner loop where the remaining unsorted elements are sorted.\n
         Time/Space Complexity is at worst O(n^2).
         """
-        last_idx = len(list)-1 #Last element index position
-        for pass_number in range(last_idx, 0, -1): #Passes through each element of the list, from the back
+        last_idx = len(list) - 1  # Last element index position
+        for pass_number in range(last_idx, 0, -1):  # Passes through each element of the list, from the back
             for idx in range(pass_number):
-                if list[idx] > list[idx+1]:
-                    list[idx], list[idx+1] = list[idx+1], list[idx]
+                if list[idx] > list[idx + 1]:
+                    list[idx], list[idx + 1] = list[idx + 1], list[idx]
         return list
 
     def merge_sort(self, list: list):
         """
         
         """
-        if len(list) > 1: #Allows recursion on list to break after split gets to 1.
-            mid = len(list)//2 #Find the middle index position of the list
-            left = list[:mid] #Separate the left side of list
-            right = list[mid:] #separte the right side of list
-            self.merge_sort(left) # Recursively sort left list until only one is left
-            self.merge_sort(right) # Recursively sort right list until only one is left
-            a = 0 # Left indexer
-            b = 0 # Right indexer
-            c = 0 # List indexer
+        if len(list) > 1:  # Allows recursion on list to break after split gets to 1.
+            mid = len(list) // 2  # Find the middle index position of the list
+            left = list[:mid]  # Separate the left side of list
+            right = list[mid:]  # separte the right side of list
+            self.merge_sort(left)  # Recursively sort left list until only one is left
+            self.merge_sort(right)  # Recursively sort right list until only one is left
+            a = 0  # Left indexer
+            b = 0  # Right indexer
+            c = 0  # List indexer
             while a < len(left) and b < len(right):
-                if left[a] < right[b]: # If first value on left is less than first value on right
-                    list[c] = list[a] # Set the first value of the list equal to the first value on left
-                    a+=1 # Add one to the index position on the left and repeat the process
-                else: # If the right is greater than the left
-                    list[c] = right[b] # Set the first value of the list to the first value on the right
-                    b+=1 # Add one to the index position on the right and repeat the process
-                c+=1 # Add one to index position of list for next iteration
+                if left[a] < right[b]:  # If first value on left is less than first value on right
+                    list[c] = list[a]  # Set the first value of the list equal to the first value on left
+                    a += 1  # Add one to the index position on the left and repeat the process
+                else:  # If the right is greater than the left
+                    list[c] = right[b]  # Set the first value of the list to the first value on the right
+                    b += 1  # Add one to the index position on the right and repeat the process
+                c += 1  # Add one to index position of list for next iteration
 
             while a < len(left):
-                list[c]=left[a]
-                a+=1
-                c+=1
+                list[c] = left[a]
+                a += 1
+                c += 1
             while b < len(right):
                 list[c] = right[b]
-                b+=1
-                c+=1
+                b += 1
+                c += 1
         return list
 
     def insertion_sort(self, list: list):
@@ -58,13 +62,13 @@ class Sorting_Algo:
         Can be used on smaller data structures, but would not be recommended for larger structures\n
         At best, if a list is already sorted, O(n), worst case is O(n^2)
         """
-        for i in range(1, len(list)): # Used to iterate through the entire list
-            j = i-1 #Selects first element of the list
-            next_element = list[i] #Selects second element of the list
-            while (list[j] > next_element) and (j >=0):
-                list[j+1] = list[j]
-                j = j-1
-            list[j+1] = next_element
+        for i in range(1, len(list)):  # Used to iterate through the entire list
+            j = i - 1  # Selects first element of the list
+            next_element = list[i]  # Selects second element of the list
+            while (list[j] > next_element) and (j >= 0):
+                list[j + 1] = list[j]
+                j = j - 1
+            list[j + 1] = next_element
         return list
 
     def shell_sort(self, list):
@@ -76,24 +80,25 @@ class Sorting_Algo:
             for i in range(dist, len(list)):
                 temp = list[i]
                 j = i
-                while j >= dist and list[j-dist] > temp:
-                    list[j] = list[j-dist]
-                    j = j-dist
+                while j >= dist and list[j - dist] > temp:
+                    list[j] = list[j - dist]
+                    j = j - dist
                 list[j] = temp
-            dist = dist//2
+            dist = dist // 2
         return list
 
     def selection_sort(self, list: list):
         """
 
         """
-        for fill_slot in range(len(list) - 1, 0, -1): #Starts from the back of the list
-            max_index=0
-            for location in range(1, fill_slot+1):
+        for fill_slot in range(len(list) - 1, 0, -1):  # Starts from the back of the list
+            max_index = 0
+            for location in range(1, fill_slot + 1):
                 if list[location] > list[max_index]:
                     max_index = location
             list[fill_slot], list[max_index] = list[max_index], list[fill_slot]
         return list
+
 
 class search_algo:
     def __init__(self):
@@ -109,13 +114,13 @@ class search_algo:
         index = 0
         found = False
         # Match the value with each data element
-        while index <len(list) and found is False:
+        while index < len(list) and found is False:
             if list[index] == item:
-                found=True
+                found = True
             else:
                 index = index + 1
         return found
-    
+
     def Binary_Search(self, list: list, item):
         """
         At each iteration, splits the data into two parts. \n
@@ -133,6 +138,7 @@ class search_algo:
             else:
                 first = midpoint + 1
         return found
+
     def Interpolation_Search(list, x):
         """
         Uses the target value to estimate the position of the element in the sorted array.\n
@@ -141,29 +147,30 @@ class search_algo:
         BigO: Best: O(log(logN)), Worst: O(N)
         """
         index_0 = 0
-        index_last = (len(list)-1)
+        index_last = (len(list) - 1)
         found = False
 
         while index_0 <= index_last and x >= list[index_0] and x <= list[index_last]:
             # Find the midpoint
-            mid = index_0 + int(((float(index_last - index_0)/(list[index_last] - list[index_0]))*(x-list[index_0])))
+            mid = index_0 + int(
+                ((float(index_last - index_0) / (list[index_last] - list[index_0])) * (x - list[index_0])))
             # Compare the value at the middle with search value
             if list[mid] == x:
                 found = True
                 return found
             if list[mid] < x:
-                index_0 = mid +1
+                index_0 = mid + 1
         return found
 
 
 class helper_functions:
     def __init__(self):
         pass
-        
+
     def null_count(self, df):
         return df.isna().sum().sum()
 
-    def tts(self, frac:float, df: pd.DataFrame):
+    def tts(self, frac: float, df: pd.DataFrame):
         """
         Input:\n Fraction, float object. Indicative of training size\n
         df: DataFrame Object. Object to split\n\n
@@ -178,29 +185,29 @@ class helper_functions:
         Input: DataFrame Object.\n
         Output: Randomized DataFrame
         """
-        return df.sample(frac = 1, random_state = seed)
+        return df.sample(frac=1, random_state=seed)
 
-    def list_2_series(self,list_2_series, df):
+    def list_2_series(self, list_2_series, df):
         """
         
         """
         ser = pd.Series(list_2_series)
-        df['list'] = df.append(ser, ignore_index = True)
+        df['list'] = df.append(ser, ignore_index=True)
         return df
 
-    def random_phrase(self, first_word_list: list, second_word_list:list):
+    def random_phrase(self, first_word_list: list, second_word_list: list):
         """
         
         """
         return f"{first_word_list[np.random.randint(0, len(first_word_list))]} {second_word_list[np.random.randint(0, len(second_word_list))]}"
 
-    def random_float(self, min_val:float, max_val:float):
+    def random_float(self, min_val: float, max_val: float):
         """
         
         """
         return np.random.uniform(min_val, max_val)
-    
-    def abbr_2_state(self, state_series : pd.Series, abbr_2_state : bool =True):
+
+    def abbr_2_state(self, state_series: pd.Series, abbr_2_state: bool = True):
         """
         Input: Series of State Acronyms or names\n
         Output: The inverse of the input: either Acronyms or Names\n\n
@@ -219,53 +226,53 @@ class helper_functions:
         else:
             state_series = state_series.map(state_dict)
             return state_series
-    
-    def split_dates(date_series : pd.Series):
+
+    def split_dates(date_series: pd.Series):
         """
         
         """
         date_series = pd.to_datetime(date_series)
-        return pd.DataFrame(data = {
-            'Day' : date_series.dt.day,
-            'Month' : date_series.dt.month,
+        return pd.DataFrame(data={
+            'Day': date_series.dt.day,
+            'Month': date_series.dt.month,
             'Year': date_series.dt.year})
-    
-    def rm_outlier(self, df:pd.DataFrame):
+
+    def rm_outlier(self, df: pd.DataFrame):
         """
         Input: DataFrame Object\n
         Output: Removes outliers from a dataframe object
         """
         return df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
-    
-    def factors(self, num:int):
+
+    def factors(self, num: int):
         """
         Input: Single positive integer\n
         Output: All factors of num in range of 1 -> num \n
         """
         if num > 0 and num.is_integer():
-            for i in range(1, num+1):
+            for i in range(1, num + 1):
                 if num % i == 0:
                     print(i)
-    
-    def multi_table(self, num:int):
+
+    def multi_table(self, num: int):
         """
         Input: Single positive integer \n
         Output : Multiplication table from 1-10
         """
         for i in range(1, 11):
-            print(f'{num} x {i} = {num*i:.2f}') # :.2f adds 2 zeros at the end
-    
+            print(f'{num} x {i} = {num * i:.2f}')  # :.2f adds 2 zeros at the end
+
     def cel_fah_conversion(self, f):
         """
         Converts fahrenheit to celsius
         """
-        return ((f - 32) * (5/9))
-    
+        return ((f - 32) * (5 / 9))
+
     def fah_cel_conversion(self, c):
         """
         Converts celsius to fahrenheit
         """
-        return (c * (9/5) + 32)
+        return (c * (9 / 5) + 32)
 
     def quadratic_formula(self, a, b, c):
         """
@@ -275,7 +282,7 @@ class helper_functions:
         Output: x1 and x2 from a quadratic equation\n
         Magic: Calculates the +- values of a quadratic equation. 
         """
-        base = (b**2 - 4 * a * c)**0.5
-        x1 = (-b + base)/(2 * a)
-        x2 = (-b - base)/(2 * a)
+        base = (b ** 2 - 4 * a * c) ** 0.5
+        x1 = (-b + base) / (2 * a)
+        x2 = (-b - base) / (2 * a)
         return x1, x2
